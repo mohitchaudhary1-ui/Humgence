3efd2bd43fc35676b1dbba7bb115163ec4ca25c3
Amandeep Singh <iamamandeepsingh2000@gmail.com>
2026-02-13 18:25:52 +0530

    make changes in service section

--- src/pages/components/Services.tsx
+++ src/pages/components/Services.tsx
@@ -45,7 +45,7 @@ export default function HomeServices() {
                 </div>
 
                 {/* --- SERVICES GRID --- */}
-                <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5 mb-16">
+                <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-4 gap-5 mb-16">
                     {featuredServices.map((service, i) => (
                         <div
                             key={i}
