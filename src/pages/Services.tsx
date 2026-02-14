45594bba7eb02c2a34fa90349a23aa7de83bc07f
Amandeep Singh <iamamandeepsingh2000@gmail.com>
2026-02-14 14:45:02 +0530

    add new service professinal shoot

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -160,6 +160,17 @@ export default function ServicesPage() {
                         "Interactive navigation"
                     ],
                     icon: "M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                },
+                {
+                    title: "Professional Shoot",
+                    points: [
+                        "High-quality photo & video production",
+                        "Reels & ad creatives for marketing use",
+                        "Indoor & outdoor shoot setups",
+                        "Professional editing & colour grading",
+                        "Brand-focused visual storytelling",
+                    ],
+                    icon: "M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
                 }
             ]
         }
